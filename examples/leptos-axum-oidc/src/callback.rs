ad41412a4c1293c1473912d281b1439c24a0799c
Leonard Marschke <leonard@marschke.me>
2026-09-02 17:32:58 +0200

    remove: leptops-axum example as it did not work properly and can be replace by pure TS/JS apps

--- examples/leptos-axum-oidc/src/callback.rs
+++
@@ -1,7 +0,0 @@
-#[leptos::component]
-pub fn Oidc() -> impl leptos::IntoView {
-    leptos::view! {
-        <h1>"Authenticating..."</h1>
-        <p>"You will be redirected in a second..."</p>
-    }
-}
